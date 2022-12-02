464904090edfbe95dd213a0126fc1b66ad038e88
Zachary Hart <zach@zthart.me>
2022-12-01 22:58:48 -0800

    housekeeping: add pyproject.toml for black config
    - formatting for day1/solution.py

--- day1/solution.py
+++ day1/solution.py
@@ -4,10 +4,11 @@ import os
 import heapq
 import functools
 
-class ElfSnacks():
+
+class ElfSnacks:
     def __init__(self):
         self._heap = []
-    
+
     def add_elf_cals(self, cals):
         heapq.heappush(self._heap, cals)
 
@@ -15,23 +16,29 @@ class ElfSnacks():
         return self.get_sum_of_top_n(1)
 
     def get_sum_of_top_n(self, n):
-        return functools.reduce(lambda acc, n: acc+n, heapq.nlargest(n, self._heap))
+        return functools.reduce(
+            lambda acc, n: acc + n, heapq.nlargest(n, self._heap)
+        )
+
 
 def main():
     snacks = ElfSnacks()
     running_total = 0
-    with open(os.path.join(os.path.dirname(__file__), 'input.txt'), 'r') as in_file:
+    with open(
+        os.path.join(os.path.dirname(__file__), "input.txt"), "r"
+    ) as in_file:
         for line in in_file:
-            if (cals := line.rstrip()) != '':
+            if (cals := line.rstrip()) != "":
                 running_total += int(cals)
-                pass
             else:
                 snacks.add_elf_cals(running_total)
                 running_total = 0
-    
-    return snacks
-    
 
-if __name__ == '__main__':
+    return snacks
+
+
+if __name__ == "__main__":
     all_snacks = main()
-    print(f'top:\t{all_snacks.get_top_cals()}\ntop 3:\t{all_snacks.get_sum_of_top_n(3)}')
+    print(
+        f"top:\t{all_snacks.get_top_cals()}\ntop 3:\t{all_snacks.get_sum_of_top_n(3)}"
+    )
